01e8b790511ca50e261e75e178a43672baac39f3
Pawel <plinek@rocketmail.com>
2020-08-03 13:37:59 -0400

    Second Class

---
+++ testProject/src/main/java/packagee/SecondClass.java
@@ -0,0 +1,10 @@
+package packagee;
+
+public class SecondClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is from the Second Class.");
+	}
+
+}
