9c69c71b2afa708cb0f3d963d1fb9074b8e9287d
Pawel <plinek@rocketmail.com>
2020-08-03 13:32:31 -0400

    First Commite From Eclipse

---
+++ testProject/src/main/java/packagee/FirstClassInPackage.java
@@ -0,0 +1,15 @@
+package packagee;
+
+public class FirstClassInPackage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println("sout + CTRL + SPACE");
+		System.out.println("Java Go Crazy.");
+		//syso (ctrl + space) = 
+		//System.out.println();
+	
+	}
+
+}
